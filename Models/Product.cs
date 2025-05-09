37d043cad283f4b1ab1f0ca32c7161037886e4c5
valenuss <valepadill597@gmail.com>
2025-05-08 22:41:22 -0500

    Se realiza la clase product

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,6 +1,21 @@
-﻿namespace Super_Market_web.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Super_Market_web.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+
+        public decimal Price { get; set; }
+
+        public int stock { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
     }
 }
