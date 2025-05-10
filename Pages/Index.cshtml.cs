881754e06675293d9353f9df4a8f17422d6727ed
valenuss <valepadill597@gmail.com>
2025-05-10 16:51:23 -0500

    Se ajusta el index

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,26 +1,29 @@
-<<<<<<< HEAD
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Super_Market_web.Data;
+using SupermarketWEB.Models;
+using SuperMarketWEB.Data;
+using SuperMarketWEB.Models;
 
 namespace Super_Market_web.Pages
-=======
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace SuperMarketWEB.Pages
->>>>>>> d0a7c64 (Se ajusta el NuGet)
-{
     public class IndexModel : PageModel
+{
+    private readonly SuperMarketContext _context;
+
+    public IndexModel(SuperMarketContext context)
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-
-        }
+        context = context;
     }
+
+    public IList<Category> categories { get; set; } = default!;
+
+    public async Task OnGetAsync()
+    {
+        if (_context.Categories != null)
+        {
+            categories = await _context.Categories.ToListAsync();
+      }
+    }   
+  }
 }
