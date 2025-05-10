6ecd3c65077688e6c188c47fad7d9e95e4d63e3e
valenuss <valepadill597@gmail.com>
2025-05-10 15:52:55 -0500

    Se edita el appsetting.json

--- Data/SuperMarketContext.cs
+++ Data/SuperMarketContext.cs
@@ -1,6 +1,18 @@
-﻿namespace Super_Market_web.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Super_Market_web.Models;
+using SupermarketWEB.Models;
+
+
+namespace Super_Market_web.Data
 {
     public class SuperMarketContext
     {
+        public SuperMarketContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> categories { get; set; } 
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,13 +1,15 @@
-﻿namespace Super_Market_web.Models
+﻿using Super_Market_web.Models;
+
+namespace SupermarketWEB.Models
 {
     public class Category
     {
-        public int Id { get; set; } //sera la llave primaria
+        public int Id { get; set; } // Será la llave primaria
 
         public string Name { get; set; }
 
         public string? Description { get; set; }
 
-        public Icollection<Product> Products { get; set; } //propiedad de navegacion
+        public ICollection<Product>? Products { get; set; } // Propiedad de navegación
     }
 }
