00538a4a4b62c916e8d39cbf07d6e65775c68f9e
valenuss <valepadill597@gmail.com>
2025-05-10 08:46:20 -0500

    Se crea la clase Category

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,5 +2,12 @@
 {
     public class Category
     {
+        public int Id { get; set; } //sera la llave primaria
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public Icollection<Product> Products { get; set; } //propiedad de navegacion
     }
 }
