2d677d154066383344a4bc15b9c7a07e60e5d378
Miguel angel serrano <valepadill597@gmail.com>
2025-05-14 18:20:00 -0500

    Se crea el archivo Create y Delete en PayModes

---
+++ Pages/PayModes/Create.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.PayModes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+        public CreateModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public PayMode PayMode { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.PayModes == null || PayMode == null)
+            {
+                return Page();
+            }
+
+            _context.PayModes.Add(PayMode);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
