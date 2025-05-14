54a51ff115e697bf4fb871c13b65611a9bcc1e63
Miguel angel serrano <valepadill597@gmail.com>
2025-05-14 18:40:33 -0500

    Se crea el archivo de Products

---
+++ Pages/Products/Delete.cshtml.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Products
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+        public DeleteModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Products.FirstOrDefaultAsync(m => m.Id == id);
+
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+
+            if (Product != null)
+            {
+                Product = product;
+                _context.Products.Remove(Product);
+                await _context.SaveChangesAsync();
+
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
